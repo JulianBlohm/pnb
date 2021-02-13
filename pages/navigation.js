af9892e89887470f22a9e402c99766d1198936cf
julian blohm <julianscript@outlook.de>
2021-02-13 16:36:26 +0100

    Add logic for navigation closing icon and position

--- pages/navigation.js
+++ pages/navigation.js
@@ -1,12 +1,15 @@
 import styles from '../styles/Navigation.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export default function Navigation() {
+    const router = useRouter()
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
-                <Link href="/navigation">
+                <a onClick={() => router.back()}>
                     <Image
                         className={styles.star}
                         src="/x.svg"
@@ -14,15 +17,23 @@ export default function Navigation() {
                         width={19.37}
                         height={18.32}
                     />
-                </Link>
+                </a>
             </header>
 
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
-                    <li><Link href="/">home</Link></li>
-                    <li><Link href="/about">about</Link></li>
-                    <li><Link href="/stories">stories</Link></li>
-                    <li><Link href="/templates">templates</Link></li>
+                    <li>
+                        <Link href="/">home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">about</Link>
+                    </li>
+                    <li>
+                        <Link href="/stories">stories</Link>
+                    </li>
+                    <li>
+                        <Link href="/templates">templates</Link>
+                    </li>
                 </ul>
             </nav>
 
